c81a665edf35b6132c834b6b343c304d5e82750a
Jaywin <jaywin.huang@gmail.com>
2017-11-11 14:27:46 -0500

    first try out

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = "mysql://root:admin@localhost:3306/obs"
---
+++ run.py
@@ -0,0 +1,3 @@
+from devobs import app
+app.run(debug=True)
+
