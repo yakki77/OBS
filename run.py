b1a949097e6192331e214a7c3440c5e7fcc72572
yakki77 <yachnepeng@gmail.com>
2017-11-12 13:22:46 +0800

    delete the old folder

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-from devobs import app
-app.run(debug=True)
-
+#!/usr/bin/env python
+# coding=utf-8
+from app import app, db
+from app.models import User, Transaction, Account
+if __name__ == '__main__':
+    #db.drop_all()
+    db.create_all()
+    app.run(debug=True)
